e1579d22d351d89eb5204c294f5fa16d2fbc709d
Bim Williams <bim@Mr-Bims-MacBook-Pro.local>
2019-02-03 13:21:36 +0000

    sorting ouut nav bar

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,41 @@
 import Link from 'next/link'
 
 const linkStyle = {
-  marginRight: 15
+  marginRight: 15,
+  textDecoration: 'none',
+  color: '#fff'
+}
+
+const headerStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  padding: '12px',
+  backgroundColor: '#765b9d',
+  color: '#fff',
+  fontFamily:'Arial'
 }
 
 const Header = () => (
-    <div>
-        <Link href="/">
-          <a style={linkStyle}>Home</a>
-        </Link>
-        <Link href="/software">
-          <a style={linkStyle}>Software</a>
-        </Link>
-        <Link href="/resources">
-          <a style={linkStyle}>Resources</a>
-        </Link>
+    <div style={headerStyle}>
+        <div className="logo-holder">
+          <Link href="/">
+            <a style={linkStyle}>D4S</a>
+          </Link>
+        </div>
+
+
+        <div className="nav">
+          <Link href="/">
+            <a style={linkStyle}>Home</a>
+          </Link>
+          <Link href="/software">
+            <a style={linkStyle}>Software</a>
+          </Link>
+          <Link href="/resources">
+            <a style={linkStyle}>Resources</a>
+          </Link>
+        </div>        
+        
     </div>
 )
 
